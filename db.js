cb84ab3848167f034db880d27eb3e31b8ae2bd9f
Dmonster <pithwanarendra29@gmail.com>
2024-10-03 01:04:17 +0530

    filteradded

---
+++ db.js
@@ -0,0 +1,579 @@
+data = [
+    {
+        "id": "1",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image1.png",
+        "alt": "",
+        "category": [
+            "general"
+        ]
+    },
+    {
+        "id": "2",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image2.png",
+        "alt": "",
+        "category": [
+            "general"
+        ]
+    },
+    {
+        "id": "3",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image3.png",
+        "alt": "",
+        "category": [
+            "general",
+            "3d"
+        ]
+    },
+    {
+        "id": "4",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image4.png",
+        "alt": "",
+        "category": [
+            "general",
+            "3d"
+        ]
+    },
+    {
+        "id": "5",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image5.png",
+        "alt": "",
+        "category": [
+            "general"
+        ]
+    },
+    {
+        "id": "6",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image6.png",
+        "alt": "",
+        "category": [
+            "general",
+            "2d"
+        ]
+    },
+    {
+        "id": "7",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image7.png",
+        "alt": "",
+        "category": [
+            "general"
+        ]
+    },
+    {
+        "id": "8",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image8.png",
+        "alt": "",
+        "category": [
+            "general",
+            "textures"
+        ]
+    },
+    {
+        "id": "9",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image9.png",
+        "alt": "",
+        "category": [
+            "general"
+        ]
+    },
+    {
+        "id": "10",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image10.png",
+        "alt": "",
+        "category": [
+            "general",
+            "3d"
+        ]
+    },
+    {
+        "id": "11",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image11.png",
+        "alt": "",
+        "category": [
+            "general"
+        ]
+    },
+    {
+        "id": "12",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image12.png",
+        "alt": "",
+        "category": [
+            "general",
+            "textures"
+        ]
+    },
+    {
+        "id": "13",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image13.png",
+        "alt": "",
+        "category": [
+            "general"
+        ]
+    },
+    {
+        "id": "14",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image14.png",
+        "alt": "",
+        "category": [
+            "general"
+        ]
+    },
+    {
+        "id": "15",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image15.png",
+        "alt": "",
+        "category": [
+            "general",
+            "abstract"
+        ]
+    },
+    {
+        "id": "16",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image16.png",
+        "alt": "",
+        "category": [
+            "general"
+        ]
+    },
+    {
+        "id": "17",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image17.png",
+        "alt": "",
+        "category": [
+            "general",
+            "abstract"
+        ]
+    },
+    {
+        "id": "18",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image18.png",
+        "alt": "",
+        "category": [
+            "general"
+        ]
+    },
+    {
+        "id": "19",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image19.png",
+        "alt": "",
+        "category": [
+            "general"
+        ]
+    },
+    {
+        "id": "20",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image20.png",
+        "alt": "",
+        "category": [
+            "general",
+            "3d"
+        ]
+    },
+    {
+        "id": "21",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image21.png",
+        "alt": "",
+        "category": [
+            "general",
+            "3d",
+            "abstract"
+        ]
+    },
+    {
+        "id": "20",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image22.png",
+        "alt": "",
+        "category": [
+            "general",
+        ]
+    },
+    {
+        "id": "23",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image23.png",
+        "alt": "",
+        "category": [
+            "general"
+        ]
+    },
+    {
+        "id": "24",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image24.png",
+        "alt": "",
+        "category": [
+            "general",
+            "textures"
+        ]
+    },
+    {
+        "id": "25",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image25.png",
+        "alt": "",
+        "category": [
+            "general"
+        ]
+    },
+    {
+        "id": "26",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image26.png",
+        "alt": "",
+        "category": [
+            "general",
+            "2d"
+        ]
+    },
+    {
+        "id": "27",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image27.png",
+        "alt": "",
+        "category": [
+            "general",
+            "2d"
+        ]
+    },
+    {
+        "id": "28",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image28.png",
+        "alt": "",
+        "category": [
+            "general",
+            "abstract"
+        ]
+    },
+    {
+        "id": "29",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image29.png",
+        "alt": "",
+        "category": [
+            "general"
+        ]
+    },
+    {
+        "id": "30",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image30.png",
+        "alt": "",
+        "category": [
+            "general"
+        ]
+    },
+    {
+        "id": "31",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image31.png",
+        "alt": "",
+        "category": [
+            "general"
+        ]
+    },
+    {
+        "id": "32",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image32.png",
+        "alt": "",
+        "category": [
+            "general",
+            "3d"
+        ]
+    },
+    {
+        "id": "33",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image33.png",
+        "alt": "",
+        "category": [
+            "general"
+        ]
+    },
+    {
+        "id": "34",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image34.png",
+        "alt": "",
+        "category": [
+            "general"
+        ]
+    },
+    {
+        "id": "35",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image35.png",
+        "alt": "",
+        "category": [
+            "general"
+        ]
+    },
+    {
+        "id": "36",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image36.png",
+        "alt": "",
+        "category": [
+            "general",
+            "textures"
+        ]
+    },
+    {
+        "id": "37",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image37.png",
+        "alt": "",
+        "category": [
+            "general",
+            "3d"
+        ]
+    },
+    {
+        "id": "38",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image38.png",
+        "alt": "",
+        "category": [
+            "general",
+            "3d"
+        ]
+    },
+    {
+        "id": "39",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image39.png",
+        "alt": "",
+        "category": [
+            "general",
+            "2d"
+        ]
+    },
+    {
+        "id": "40",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image40.png",
+        "alt": "",
+        "category": [
+            "general",
+            "3d"
+        ]
+    },
+    {
+        "id": "41",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image41.png",
+        "alt": "",
+        "category": [
+            "general",
+            "abstract"
+        ]
+    },
+    {
+        "id": "42",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image42.png",
+        "alt": "",
+        "category": [
+            "general"
+        ]
+    },
+    {
+        "id": "43",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image43.png",
+        "alt": "",
+        "category": [
+            "general"
+        ]
+    },
+    {
+        "id": "44",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image44.png",
+        "alt": "",
+        "category": [
+            "general",
+            "textures"
+        ]
+    },
+    {
+        "id": "45",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image45.png",
+        "alt": "",
+        "category": [
+            "general"
+        ]
+    },
+    {
+        "id": "46",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image46.png",
+        "alt": "",
+        "category": [
+            "general",
+            "2d"
+        ]
+    },
+    {
+        "id": "47",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image47.png",
+        "alt": "",
+        "category": [
+            "general",
+            "abstract"
+        ]
+    },
+    {
+        "id": "48",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image48.png",
+        "alt": "",
+        "category": [
+            "general"
+        ]
+    },
+    {
+        "id": "49",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image49.png",
+        "alt": "",
+        "category": [
+            "general",
+            "abstract"
+        ]
+    },
+    {
+        "id": "50",
+        "title": "",
+        "artist": "",
+        "description": "",
+        "src": "images/img/image50.png",
+        "alt": "",
+        "category": [
+            "general",
+            "3d"
+        ]
+    }
+]
